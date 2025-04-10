7fc55302caf0cc07edb6f327f99ee95538b2c1bc
Jiri Daněk <jdanek@redhat.com>
2025-04-10 22:42:10 +0200

    Continue provisioning in Python

--- components/deploy.py
+++ components/deploy.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
+import os
 import contextlib
 import sys
 import subprocess
+from typing import Callable
 
 
 def main():
+    tf = TestFrame()
+
     with gha_log_group("Install ArgoCD CLI"):
         ARGOCD_VERSION = "v2.14.9"
         sh(f"curl -sSL -o /tmp/argocd-{ARGOCD_VERSION} https://github.com/argoproj/argo-cd/releases/download/{ARGOCD_VERSION}/argocd-linux-amd64")
@@ -22,11 +28,45 @@ def main():
 
         sh("oc version")
 
+    # https://istio.io/latest/docs/setup/platform-setup/kind/
+    # https://istio.io/latest/docs/tasks/traffic-management/ingress/gateway-api/#setup
+    # https://ryandeangraham.medium.com/istio-gateway-api-nodeport-c598a21c4c95
+    with gha_log_group("Install Istio"):
+        ISTIO_VERSION = "1.25.1"
+        TARGET_ARCH = "x86_64"
 
-def sh(cmd: str):
+        # TLSRoute is considered "experimental"
+        # https://github.com/kubernetes-sigs/gateway-api/issues/2643
+        sh('kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+          { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v1.2.1" | kubectl apply -f -; }')
+
+        sh("curl -L https://istio.io/downloadIstio | sh -", env={
+            "ISTIO_VERSION": ISTIO_VERSION,
+            "TARGET_ARCH": TARGET_ARCH,
+        })
+        sh(f"istio-{ISTIO_VERSION}/bin/istioctl install --set values.pilot.env.PILOT_ENABLE_ALPHA_GATEWAY_API=true --set profile=minimal -y")
+
+        sh("kubectl apply -f components/06-gateway.yaml")
+
+        tf.defer(None, lambda _: sh(
+            "kubectl wait -n istio-system --for=condition=programmed gateways.gateway.networking.k8s.io gateway"))
+    # export INGRESS_HOST=$(kubectl get gateways.gateway.networking.k8s.io gateway -n istio-system -ojsonpath='{.status.addresses[0].value}')
+
+    with tf:
+        pass
+
+
+def sh(cmd: str, env: dict[str, str] | None = None):
     """Runs a shell command."""
+    env = env or {}
     print(f"$ {cmd}", file=sys.stdout)
-    subprocess.check_call(cmd, shell=True)
+    sys.stdout.flush()
+    subprocess.check_call(
+        "set -Eeuxo pipefail; " + cmd,
+        shell=True, executable="/bin/bash",
+        env={**os.environ, **env}
+    )
+    sys.stdout.flush()
 
 
 # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
@@ -40,6 +80,22 @@ def gha_log_group(title: str) -> None:
     finally:
         print("::endgroup::", file=sys.stdout)
         sys.stdout.flush()
+
+
+class TestFrame:
+    def __init__(self):
+        self.stack = []
+
+    def defer[T](self, obj: T, fn: Callable[[T], None]):
+        self.stack.append((obj, fn))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        while self.stack:
+            obj, fn = self.stack.pop()
+            fn(obj)
 
 
 if __name__ == "__main__":
